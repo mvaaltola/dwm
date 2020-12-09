202142740bb4a8693d4d2e28ec4c29953f231120
Mikael Vaaltola <mvaaltola@gmail.com>
2020-12-09 16:02:39 +0200

    Change quit keybind to prevent accidental close

--- config.h
+++ config.h
@@ -112,7 +112,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_F4,     quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 };
 
