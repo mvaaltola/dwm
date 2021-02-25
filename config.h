a40b0d4aa410687266fa8b41a1612723e79293a9
Mikael Vaaltola <mvaaltola@gmail.com>
2021-02-25 22:06:16 +0200

    Use transparent statusbar

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
-static const unsigned int baralpha = 0xd0;
+static const unsigned int baralpha = 0;
 static const unsigned int borderalpha = OPAQUE;
 static char *colors[][3] = {
        /*               fg           bg           border   */
@@ -120,8 +120,8 @@ static const char *ffscratchpadcmd[] = {"w", "firefox", NULL};
 ResourcePref resources[] = {
 		{ "color0",             STRING,  &normbgcolor },
 		{ "color0",             STRING,  &normbordercolor },
-		{ "color4",             STRING,  &normfgcolor },
-		{ "color4",             STRING,  &selbgcolor },
+		{ "color0",             STRING,  &normfgcolor },
+		{ "color0",             STRING,  &selbgcolor },
 		{ "color8",             STRING,  &selbordercolor },
 		{ "color0",             STRING,  &selfgcolor },
 		{ "borderpx",          	INTEGER, &borderpx },
